a3ad62b585c2fefa16de0ebc27e31dc3d49f4ece
XueTao <451831819@qq.com>
2017-02-16 09:12:12 +0800

    Initial Commit

---
+++ 系统条件编译宏/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  系统条件编译宏
+//
+//  Created by 薛涛 on 17/2/16.
+//  Copyright © 2017年 Xuetao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 系统条件编译宏/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  系统条件编译宏
+//
+//  Created by 薛涛 on 17/2/16.
+//  Copyright © 2017年 Xuetao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
